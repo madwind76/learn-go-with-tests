f9f944de29daea4c2eb8c2dcba407cef16afa00d
kisec <madwind7@gmail.com>
2019-03-29 20:51:40 +0900

    Update structs

---
+++ structs/shapes_test.go
@@ -0,0 +1,26 @@
+package shapes
+
+import "testing"
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.hasArea)
+			}
+
+		})
+	}
+
+}
